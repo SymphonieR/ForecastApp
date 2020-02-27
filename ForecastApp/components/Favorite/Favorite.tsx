efac250d4ad6ae79c5f664674a91c1571a53a4d7
SymphonieR <symphonie@gatech.edu>
2020-02-27 13:28:19 +0100

    WIP

---
+++ ForecastApp/components/Favorite/Favorite.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export class Favorite extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Favorites</Text>
+            </View>
+        )
+    }
+}
